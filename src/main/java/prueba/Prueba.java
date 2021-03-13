71c3bbc4c1b36371195a5bac6be9ae10798d2aec
Bibi <huitzilopochtlik@gmail.com>
2021-03-13 10:38:54 +0100

    prueba

--- src/main/java/prueba/Prueba.java
+++ src/main/java/prueba/Prueba.java
@@ -1,8 +1,25 @@
 package prueba;
 
+import jdk.dynalink.linker.LinkerServices;
+
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+
+import java.util.List;
+import java.util.Queue;
+
 public class Prueba {
 
     public static void main(String[] args) {
+
+        Queue<String> names = new LinkedList<>();
+
+        names.add("Hello");
+        names.add("Bibo");
+        names.add("Tiva");
+
+        System.out.println(names.element());
+
         System.out.println("Prueba");
     }
 }
